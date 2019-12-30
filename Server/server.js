42c1fd7f38633d1d17687e843cbedf222e4dcfe0
Wuil <bousswuil@gmail.com>
2019-12-30 02:18:10 +0100

    console.log update

--- Server/server.js
+++ Server/server.js
@@ -5,5 +5,5 @@ const url = 'mongodb://localhost:27017/database'
 const connect = mongoose.connect(url);
 
 connect.then(db => {
-    console.log('server is running');
+    console.log('server is running now');
 }, (err) => {console.log(err)});
